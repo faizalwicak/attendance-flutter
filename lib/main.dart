a7e49c7ba47c21023dd75129c08f48038844e3dc
Faizal <faizalwicak@gmail.com>
2022-12-20 09:33:35 +0700

    add web frame

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:attendance_flutter/constant/color_constant.dart';
 import 'package:attendance_flutter/screen/home_screen.dart';
 import 'package:attendance_flutter/screen/login_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 import 'package:provider/provider.dart';
 
@@ -32,30 +34,36 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthNotifier()),
       ],
-      child: MaterialApp(
-        title: 'Attendance Flutter',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: createMaterialColor(primaryColor),
-        ),
-        localizationsDelegates: const [
-          MonthYearPickerLocalizations.delegate,
-        ],
-        home: Consumer<AuthNotifier>(
-          builder: (context, value, child) {
-            if (value.accessToken == null) {
-              return const Scaffold(
-                backgroundColor: backgroundColor,
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            } else if (value.accessToken == '') {
-              return const LoginScreen();
-            }
-            return const HomeScreen();
-          },
-        ),
+      child: FlutterWebFrame(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Attendance Flutter',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: createMaterialColor(primaryColor),
+            ),
+            localizationsDelegates: const [
+              MonthYearPickerLocalizations.delegate,
+            ],
+            home: Consumer<AuthNotifier>(
+              builder: (context, value, child) {
+                if (value.accessToken == null) {
+                  return const Scaffold(
+                    backgroundColor: backgroundColor,
+                    body: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else if (value.accessToken == '') {
+                  return const LoginScreen();
+                }
+                return const HomeScreen();
+              },
+            ),
+          );
+        },
+        maximumSize: const Size(475, 812),
+        enabled: kIsWeb,
       ),
     );
   }
