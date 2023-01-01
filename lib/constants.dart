cf6cbe70244db61a739ddef27c8aeedea4f62486
Faizal <faizalwicak@gmail.com>
2023-01-02 05:27:27 +0700

    update icon

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 // const String baseUrl = "http://192.168.1.10:8000/api";
-const String baseUrl = "http://192.168.220.44:8000";
+// const String baseUrl = "http://192.168.220.44:8000";
 
-// const String baseUrl = "https://180.214.246.117";
+const String baseUrl = "https://180.214.246.117";
 const String baseApiUrl = "$baseUrl/api";
 const dayName = [
   "Senin",
